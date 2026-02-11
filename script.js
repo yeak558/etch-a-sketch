df3f592c9d68d458d239da05e5d8e6d2acd6cdcf
yeak558 <yeak558@gmail.com>
2026-02-11 19:16:35 +0300

    Add background color, center canvas

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Select elements
 const gridContainer = document.querySelector(".grid-container");
 const pixelNumberButton = document.querySelector("#pixel-number-button");
+const canvasInfo = document.querySelector("#canvas-info");
 
 // General variables
-let defaultPixelNumber = "64"
+let defaultPixelNumber = 64;
 
 pixelNumberButton.addEventListener("click", drawCanvas);
 
@@ -11,16 +12,19 @@ pixelNumberButton.addEventListener("click", drawCanvas);
 function drawCanvas() {
 	let pixelNumber;
 	do {
-		pixelNumber = Number(prompt("Which width-height do you want?", defaultPixelNumber));
+		pixelNumber = Number(prompt("Which width-height do you want?", String(defaultPixelNumber)));
 		if (pixelNumber > 100) {
 			alert("Please enter a positive value less than or equal to 100");
 		}
 	} while (pixelNumber <= 0 || pixelNumber > 100)
 
+	canvasInfo.textContent = `Canvas size: ${pixelNumber}x${pixelNumber}`;
 	for (let i = 1; i <= pixelNumber; i++) {
 		let grid = document.createElement("div");
 		grid.classList.add("grid");
 		gridContainer.appendChild(grid);
 	}
-	defaultPixelNumber = String(pixelNumber);
+	defaultPixelNumber = pixelNumber;
 }
+
+// It adds the new grids, it should redraw, fix it
