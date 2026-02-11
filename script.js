e676987044b0c5bb55b05c2bca1f646484f41a35
yeak558 <yeak558@gmail.com>
2026-02-11 15:30:25 +0300

    Add feature: limit max pixel number

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const pixelNumberButton = document.querySelector("#pixel-number-button");
 pixelNumberButton.addEventListener("click", drawCanvas);
 
 function drawCanvas() {
-	let pixelNumber = Number(prompt("Which width-height do you want?", "64"));
+	let pixelNumber;
+	do {
+		pixelNumber = Number(prompt("Which width-height do you want?", "64"));
+		if (pixelNumber > 100) {
+			alert("Please enter a positive value less than or equal to 100");
+		}
+	} while (pixelNumber <= 0 || pixelNumber > 100)
 
 	for (let i = 1; i <= pixelNumber; i++) {
 		let grid = document.createElement("div");
