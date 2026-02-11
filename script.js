e343700a9cb21be0abfbd827c5c6b74afe53b081
yeak558 <yeak558@gmail.com>
2026-02-11 15:05:52 +0300

    Add grid style to a class, Add flex layout

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 1000; i++) {
 	let grid = document.createElement("div");
-	grid.style.border = "1px solid gray";
-	grid.style.width = "16px";
-	grid.style.height = "16px";
+	grid.classList.add("grid");
+	// grid.style.display = "inline-block";
 	gridContainer.appendChild(grid);
 }
