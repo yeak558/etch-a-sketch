43f1141ae7a1ed8d7ec9c04f4178b259706dd92a
yeak558 <yeak558@gmail.com>
2026-02-11 15:15:54 +0300

    Add hover effect

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const gridContainer = document.querySelector(".grid-container");
 
-for (let i = 1; i <= 1000; i++) {
+for (let i = 1; i <= 10000; i++) {
 	let grid = document.createElement("div");
 	grid.classList.add("grid");
-	// grid.style.display = "inline-block";
 	gridContainer.appendChild(grid);
 }
