402d7ca761b5366712bbe9ec89b1499f6ed7df63
yeak558 <yeak558@gmail.com>
2026-02-11 19:26:15 +0300

    Fix canvas center design, fix put pixel per side

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const canvasInfo = document.querySelector("#canvas-info");
 
 // General variables
 let defaultPixelNumber = 64;
+const pixelSize = 16;
 
 pixelNumberButton.addEventListener("click", drawCanvas);
 
@@ -18,8 +19,10 @@ function drawCanvas() {
 		}
 	} while (pixelNumber <= 0 || pixelNumber > 100)
 
+	gridContainer.style.width = `${pixelSize * pixelNumber}px`;
+	gridContainer.style.height = `${pixelSize * pixelNumber}px`;
 	canvasInfo.textContent = `Canvas size: ${pixelNumber}x${pixelNumber}`;
-	for (let i = 1; i <= pixelNumber; i++) {
+	for (let i = 1; i <= pixelNumber**2; i++) {
 		let grid = document.createElement("div");
 		grid.classList.add("grid");
 		gridContainer.appendChild(grid);
