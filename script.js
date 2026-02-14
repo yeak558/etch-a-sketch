221adab22bb10ba1190d967b07a3d61014acc482
yeak558 <yeak558@gmail.com>
2026-02-14 18:10:53 +0300

    Fix initCanvas function

--- script.js
+++ script.js
@@ -3,22 +3,30 @@ const gridContainer = document.querySelector(".grid-container");
 const pixelNumberButton = document.querySelector("#pixel-number-button");
 const canvasInfo = document.querySelector("#canvas-info");
 
+
 // General variables
 let defaultPixelNumber = 64;
-let pixelSize = 16;
+let pixelSize; // will be decided according to pixel number
+const width = 640;
+const height = 640;
+
 
 initCanvas();
 pixelNumberButton.addEventListener("click", drawCanvas);
 
 function initCanvas() {
-	gridContainer.style.width = `${pixelSize * 64}px`;
-	gridContainer.style.height = `${pixelSize * 64}px`;
 	canvasInfo.textContent = "Canvas size: 64x64";
-	let totalGrid = 64**2;
-	for (let i = 1; i <= totalGrid; i++) {
-		let grid = document.createElement("div");
-		grid.classList.add("grid");
-		gridContainer.appendChild(grid);
+	for (let i = 1; i <= 64; i++ ) {
+		let gridLine = document.createElement("div");
+		gridLine.classList.add("grid-line");
+		for (let j = 1; j <= 64; j++) {
+			let pixel = document.createElement("div");
+			pixel.style.width = `${width / 64}px`;
+			pixel.style.height = `${height / 64}px`;
+			pixel.classList.add("pixel");
+			gridLine.appendChild(pixel);
+		}
+		gridContainer.appendChild(gridLine);
 	}
 }
 
@@ -31,10 +39,6 @@ function drawCanvas() {
 			alert("Please enter a positive value less than or equal to 100");
 		}
 	} while (pixelNumber <= 0 || pixelNumber > 100)
-
-	// gridContainer.style.width = `${pixelSize * pixelNumber}px`;
-	// gridContainer.style.height = `${pixelSize * pixelNumber}px`;
-	
 	canvasInfo.textContent = `Canvas size: ${pixelNumber}x${pixelNumber}`;
 	for (let i = 1; i <= pixelNumber; i++) {
 		let gridLine = document.createElement("div");
